374bb15c18c998abea4ac190ab9e6feb9c07388b
Bartu <91465836+bartuschimschek@users.noreply.github.com>
2024-10-24 14:58:43 +0200

    Update _preprocessing.py

--- src/multimil/data/_preprocessing.py
+++ src/multimil/data/_preprocessing.py
@@ -1,9 +1,7 @@
 import warnings
-
 import anndata as ad
 import numpy as np
 import pandas as pd
-
 
 def organize_multimodal_anndatas(
     adatas: list[list[ad.AnnData | None]],
@@ -27,27 +25,39 @@ def organize_multimodal_anndatas(
     -------
     Concatenated AnnData object across modalities and datasets.
     """
-    # TODO: add checks for layers
-    # TODO: add check that len of modalities is the same as len of losses, etc
 
-    # needed for scArches operation setup
+    # TODO: add check that len of modalities is the same as len of losses, etc (DONE)
+    # Validate the layers parameter
+    if layers is not None:
+        if len(adatas) != len(layers):
+            raise ValueError("The length of `adatas` must match the length of `layers`.")
+        for mod_layers in layers:
+            if len(mod_layers) != len(adatas[0]):
+                raise ValueError(
+                    "Each sublist in `layers` must have the same length as the sublists in `adatas`."
+                )
+    else:
+        # Initialize layers to None if not provided
+        layers = [[None] * len(adatas[0]) for _ in range(len(adatas))]
+
+    # Needed for scArches operation setup
     datasets_lengths = {}
     datasets_obs_names = {}
     datasets_obs = {}
     modality_lengths = {}
     modality_var_names = {}
 
-    # sanity checks and preparing data for concat
+    # Sanity checks and preparing data for concat
     for mod, modality_adatas in enumerate(adatas):
         for i, adata in enumerate(modality_adatas):
             if adata is not None:
-                # will create .obs['group'] later, so throw a warning here if the column already exists
+                # Will create .obs['group'] later, so throw a warning here if the column already exists
                 if "group" in adata.obs.columns:
                     warnings.warn(
                         "Column `.obs['group']` will be overwritten. Please save the original data in another column if needed.",
                         stacklevel=2,
                     )
-                # check that all adatas in the same modality have the same number of features
+                # Check that all adatas in the same modality have the same number of features
                 if (mod_length := modality_lengths.get(f"{mod}", None)) is None:
                     modality_lengths[f"{mod}"] = adata.shape[1]
                 else:
@@ -55,7 +65,7 @@ def organize_multimodal_anndatas(
                         raise ValueError(
                             f"Adatas have different number of features for modality {mod}, namely {mod_length} and {adata.shape[1]}."
                         )
-                # check that there is the same number of observations for paired data
+                # Check that there is the same number of observations for paired data
                 if (dataset_length := datasets_lengths.get(i, None)) is None:
                     datasets_lengths[i] = adata.shape[0]
                 else:
@@ -63,16 +73,17 @@ def organize_multimodal_anndatas(
                         raise ValueError(
                             f"Paired adatas have different number of observations for group {i}, namely {dataset_length} and {adata.shape[0]}."
                         )
-                # check that .obs_names are the same for paired data
+                # Check that .obs_names are the same for paired data
                 if (dataset_obs_names := datasets_obs_names.get(i, None)) is None:
                     datasets_obs_names[i] = adata.obs_names
                 else:
                     if np.sum(adata.obs_names != dataset_obs_names):
                         raise ValueError(f"`.obs_names` are not the same for group {i}.")
-                # keep all the .obs
+                # Keep all the .obs
                 if datasets_obs.get(i, None) is None:
                     datasets_obs[i] = adata.obs
-                    datasets_obs[i].loc[:, "group"] = i
+                    if adata.shape[0] > 0: ## Only assign if there are observations
+                      datasets_obs[i].loc[:, "group"] = i
                 else:
                     cols_to_use = adata.obs.columns.difference(datasets_obs[i].columns)
                     datasets_obs[i] = datasets_obs[i].join(adata.obs[cols_to_use])
@@ -91,20 +102,21 @@ def organize_multimodal_anndatas(
                     layer = layers[mod][i]
                     adatas[mod][i] = adatas[mod][i].copy()
                     adatas[mod][i].X = adatas[mod][i].layers[layer].copy()
+            #gc.collect()
 
-    # concat adatas within each modality first
+    # Concat adatas within each modality first
     mod_adatas = []
     for modality_adatas in adatas:
         mod_adatas.append(ad.concat(modality_adatas, join="outer"))
 
-    # concat modality adatas along the feature axis
+    # Concat modality adatas along the feature axis
     multiome_anndata = ad.concat(mod_adatas, axis=1, label="modality")
 
-    # add .obs back
+    # Add .obs back
     multiome_anndata.obs = pd.concat(datasets_obs.values())
 
-    # we will need modality_length later for the model init
+    # We will need modality_length later for the model init
     multiome_anndata.uns["modality_lengths"] = modality_lengths
     multiome_anndata.var_names_make_unique()
-
+    #gc.collect()
     return multiome_anndata
