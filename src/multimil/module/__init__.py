c89f0bd77013d8e685095ebb3afa371c9dc8a5ac
Bartu <91465836+bartuschimschek@users.noreply.github.com>
2025-03-26 12:41:49 +0100

    Update __init__.py

--- src/multimil/module/__init__.py
+++ src/multimil/module/__init__.py
@@ -1,4 +1,4 @@
 from ._multivae_torch import MultiVAETorch
 from ._mil_torch import MILClassifierTorch
-
-__all__ = ["MultiVAETorch", "MILClassifierTorch"]
+from ._unsupervised_mil_torch import UnsupervisedMILTorch
+__all__ = ["MultiVAETorch", "MILClassifierTorch", "UnsupervisedMILTorch"]
