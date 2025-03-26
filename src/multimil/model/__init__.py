63b12fc9bdeb0e060ee84e33a1212af095335863
Bartu <91465836+bartuschimschek@users.noreply.github.com>
2025-03-26 12:35:07 +0100

    Update __init__.py

--- src/multimil/model/__init__.py
+++ src/multimil/model/__init__.py
@@ -1,4 +1,5 @@
-from ._mil import MILClassifier
 from ._multivae import MultiVAE
+from ._mil import MILClassifier
+from ._multivae import UnsupervisedMIL
 
-__all__ = ["MultiVAE", "MILClassifier"]
+__all__ = ["MultiVAE", "MILClassifier", "UnsupervisedMIL"]
